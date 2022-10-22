8725e24419c90a792081597623c2565d5f4710ed
christianshub <christianshub@github.com>
2022-10-22 16:59:17 +0200

    Logger position change

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import base64_decode as decoder
 from logger import *
 
 # scheme = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' # MIME, standard
-scheme = 'aABCDEFGHIJKLMNOPQRSTUVWXYZbcdefghijklmnopqrstuvwxyz0123456789+/' # index switched ('a')
+scheme = 'asbbDEFGHIJKLMNOPQRSTUVWXYZbcdefghijklmnopqrstuvwxyz0123456789=/' # index switched ('a')
 
 encoded_str_1 = encoder.encode("f", scheme)
 encoded_str_2 = encoder.encode("fo", scheme)
@@ -18,3 +18,10 @@ decoder.decode(encoded_str_3, scheme)
 decoder.decode(encoded_str_4, scheme)
 decoder.decode(encoded_str_5, scheme)
 decoder.decode(encoded_str_6, scheme)
+
+scheme = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' # index switched ('a')
+decoder.decode("bGlnaHQgd29yay4=", scheme)
+
+
+scheme = 'aABCDEFGHIJKLMNOPQRSTUVWXYZbcdefghijklmnopqrstuvwxyz0123456789+/' # index switched ('a')
+decoder.decode("c2UsYi1kYWM0cnUjdFlvbiAjb21wbFU0YP==", scheme)
