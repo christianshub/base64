d7c9cabee68893a01d5ac7ab6366eb3dcd222780
christianshub <christianshub@github.com>
2022-10-23 07:11:18 +0100

    Edit tests

--- tests.py
+++ tests.py
@@ -19,9 +19,12 @@ decoder.decode(encoded_str_4, scheme)
 decoder.decode(encoded_str_5, scheme)
 decoder.decode(encoded_str_6, scheme)
 
-scheme = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' # index switched ('a')
+scheme = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' # index switched ('a')_
 decoder.decode("bGlnaHQgd29yay4=", scheme)
 
 
 scheme = 'aABCDEFGHIJKLMNOPQRSTUVWXYZbcdefghijklmnopqrstuvwxyz0123456789+/' # index switched ('a')
 decoder.decode("c2UsYi1kYWM0cnUjdFlvbiAjb21wbFU0YP==", scheme)
+
+scheme = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' # index switched ('a')
+decoder.decode("ydN8BXq16RE=", scheme)
